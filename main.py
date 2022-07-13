65b52a0c6f0a20b38124aafc4f34f5cd08814bf5
nate <flyingwithnate@icloud.com>
2022-07-12 21:02:18 -0700

    environments for security + 2 major functions

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from twilio.rest import Client
 import finnhub
+import os
 
-finnhub_client = finnhub.Client(api_key="cb6ptfiad3idq8jbm7cg")
+
+
 
 """
 quote function:
@@ -13,4 +16,26 @@ o: Open price of the day
 pc: Previous close price
 """
 
-print(finnhub_client.quote('GME'))
+
+
+def getGMEprice():
+    finnhub_client = finnhub.Client(api_key="cb6ptfiad3idq8jbm7cg")
+    quote = finnhub_client.quote('GME')
+    return quote
+
+
+def send_message():
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    auth_token = os.environ['TWILIO_AUTH_TOKEN'] 
+    client = Client(account_sid, auth_token)
+    price = getGMEprice()
+    distanceFrom = 1000 - float(price["c"])
+    toSend = 'The price of GME is: ${}. That is ${} away from $1000 >:)'.format(price["c"], distanceFrom)
+
+    message = client.messages.create(  
+                                messaging_service_sid='MGeac4403637e61cc02e94a915313c9024', 
+                                body=toSend,      
+                                to='+19166275849' 
+                            )
+
+send_message()
